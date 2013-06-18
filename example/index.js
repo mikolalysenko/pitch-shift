d3138ccdead0de1cef5b35c22abe99beedc1c209
Mikola Lysenko <mikolalysenko@gmail.com>
2013-06-18 11:52:10 -0500

    added gettysburg address to demo

--- example/index.js
+++ example/index.js
@@ -40,36 +40,119 @@ function createProcessingNode(context) {
   return scriptNode
 }
 
+var context = new webkitAudioContext()
+var ondatasource = function(url, buf) {}
+
+var dataSources = {
+  "oscillator": function() { return context.createOscillator() }
+}
+
+function createFileSource(buf) {
+  var ret = context.createBufferSource()
+  ret.buffer = buf
+  return ret
+}
+
+
+function loadFile(url) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      dataSources[url] = createFileSource.bind(undefined, buffer)
+      ondatasource(url)
+    }, function(err) {
+      console.log("Error loading file", url, ":", err)
+    })
+  }
+  request.send()
+}
+
+function loadFiles(list) {
+  for(var i=0; i<list.length; ++i) {
+    loadFile(list[i])
+  }
+}
+
+loadFiles(["gettysburg.mp3"])
+
+var prettyNames = {
+  "oscillator": "Sine Wave",
+  "gettysburg.mp3": "Gettysburg Address"
+}
 
 domready(function() {
 
   //Init web audio
-  var context = new webkitAudioContext()
   var shifter = createProcessingNode(context)
-  shifter.connect(context.destination)
   
   var pausePlay = document.getElementById("pausePlay")
   var sourceSelect = document.getElementById("audioSource")
+  var applyShift = document.getElementById("applyShift")
+  
   var playing = false
+  var useFilter = true
   var curSource = null
   
-  var dataSources = {
-    "oscillator": context.createOscillator()
+  ondatasource = function(url) {
+    var opt = document.createElement("option")
+    opt.text = prettyNames[url]
+    opt.value = url
+    sourceSelect.add(opt)
+  }
+
+  sourceSelect.remove(0)
+  for(var id in dataSources) {
+    ondatasource(id)
   }
   
   pausePlay.addEventListener("click", function() {
     if(playing) {
-      curSource.stop(0)
-      curSource.disconnect()
+      curSource.disconnect(0)
+      if(useFilter) {
+        shifter.disconnect(0)
+      }
+      if(!curSource.start) {
+        curSource.noteOff(0)
+      } else {
+        curSource.stop(0)
+        curSource.noteOff(0)
+      }
       curSource = null
       playing = false
       pausePlay.value = "Play"
     } else {
-      curSource = dataSources[sourceSelect.value]
-      curSource.connect(shifter)
-      curSource.start(0)
+      curSource = (dataSources[sourceSelect.value])()
+      if(useFilter) {
+        curSource.connect(shifter)
+        shifter.connect(context.destination)
+      } else {
+        curSource.connect(context.destination)
+      }
+      if(!curSource.start) {
+        curSource.noteOn(0)
+      } else {
+        curSource.start(0)
+      }
+      curSource.loop = true
       playing = true
       pausePlay.value = "Pause"
+    }
+  })
+  
+  
+  applyShift.addEventListener("change", function() {
+    useFilter = !!applyShift.checked
+    if(playing) {
+      curSource.disconnect(0)
+      if(useFilter) {
+        curSource.connect(shifter)
+        shifter.connect(context.destination)
+      } else {
+        shifter.disconnect(0)
+        curSource.connect(context.destination)
+      }
     }
   })
 })
